df407e3ffee133853b3abd6d221119056bd7d584
devbeau <48367657+devbeau@users.noreply.github.com>
2020-08-14 12:29:59 -0400

    fixed search bug

--- starwars/src/components/Header.js
+++ starwars/src/components/Header.js
@@ -1,14 +1,35 @@
 import React from 'react';
 import SearchBar from './SearchBar';
+import styled from 'styled-components';
+
+let StyledHeader = styled.div`
+    
+    div{
+    height: 50px;
+    display: flex;
+
+    input{
+        width: 40%;
+        height: 65%;
+        margin: auto;
+        font-size: 2rem;
+        font-family: Cambria, Cochin, Georgia, Times, 'Times New Roman', serif;
+        text-align: center;
+        box-shadow: 0px 0px 2px 3px rgba(16,16,16,0.33);
+    }
+}
+`
 
 function Header(props) {
+
+
 
  console.log(props);
 
     return (
-        <div className="Header">
+        <StyledHeader className="Header">
             <SearchBar {...props}/>
-        </div>
+        </StyledHeader>
     );
 }
 
--- starwars/src/components/SearchBar.js
+++ starwars/src/components/SearchBar.js
@@ -3,20 +3,7 @@ import styled from 'styled-components';
 function SearchBar(props) {
 let setCharacterList = props.setCharacterList;
 
-let StyledSearch = styled.div`
-    height: 50px;
-    display: flex;
 
-    input{
-        width: 40%;
-        height: 65%;
-        margin: auto;
-        font-size: 2rem;
-        font-family: Cambria, Cochin, Georgia, Times, 'Times New Roman', serif;
-        text-align: center;
-        box-shadow: 0px 0px 2px 3px rgba(16,16,16,0.33);
-    }
-`
 
 let searchValue = props.searchValue;
     useEffect(() => {
@@ -34,14 +21,15 @@ let searchValue = props.searchValue;
 
 console.log(props);
   return (
-    <StyledSearch className="SearchBar">
+    <div className="SearchBar">
       <input type="text" 
       defaultValue="Search Characters..."
       onClick={event => event.target.value=''}
       onInput={(event) => {
           props.setSearchValue(event.target.value);
+          
       }} />
-    </StyledSearch>
+    </div>
   );
 }
 
